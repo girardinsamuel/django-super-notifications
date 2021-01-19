b6f9179e2ae61b678057c070f7cf33486d369782
Samuel Girardin <samuelgirardin@pm.me>
2021-01-19 12:33:19 +0100

    fix for django 3

--- super_notifications/api/urls.py
+++ super_notifications/api/urls.py
@@ -4,4 +4,4 @@ from rest_framework import routers
 from super_notifications.api.views import NotificationsViewSet
 
 notifications_router = routers.SimpleRouter()
-notifications_router.register(r'notifications', NotificationsViewSet, base_name='notifications-list')
+notifications_router.register(r'notifications', NotificationsViewSet, basename='notifications-list')
